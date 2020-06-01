1ceb8d581bbf9558ad5d53172c0ed07423540fc9
Marc Rivero Marti <riverole@gmail.com>
2020-05-31 22:15:36 -0700

    Fix quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-JSON_FMT='{"google": {client_id: %s, picker: %s, fonts: %s}}\n'
+JSON_FMT='{"google": {"client_id": "%s", "picker": "%s", "fonts": "%s"}}\n'
 
 printf "$JSON_FMT" "$CLIENT_ID" "$PICKER" "$PICKER" > .api_keys.dev.json
