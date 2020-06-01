11051c550e74cbf464d7637759e7c957bfa6cfd9
Marc Rivero Marti <riverole@gmail.com>
2020-05-31 22:12:39 -0700

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -l
 
 JSON_FMT='{"google": {client_id: %s, picker: %s, fonts: %s}}\n'
-JSON_API_KEYS=printf "$JSON_FMT" "$CLIENT_ID" "$PICKER" "$PICKER"
 
-echo $JSON_API_KEYS
-
-echo $JSON_API_KEYS > .api_keys.dev.json
+printf "$JSON_FMT" "$CLIENT_ID" "$PICKER" "$PICKER" > .api_keys.dev.json
