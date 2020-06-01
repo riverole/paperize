36eb395c59d71a6ef5faf79aef4cd8053f3405d3
Marc Rivero Marti <riverole@gmail.com>
2020-05-31 21:42:24 -0700

    More test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-echo "You $2"
+JSON_STRING='{"google"{"client_id":"$CLIENT_ID"","picker":"$PICKER","fonts":"$PICKER"}}'
 
-JSON_STRING='{"google"{"client_id":"$1"","picker":"$2","fonts":"$2"}}'
+echo $JSON_STRING
 
 echo $JSON_STRING > .api_keys.dev.json
